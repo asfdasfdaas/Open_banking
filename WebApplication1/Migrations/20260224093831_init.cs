6397a9a242f2abbf4a121db7e438aa48da569dc0
faruk <faruk@MSI>
2026-02-24 14:33:23 +0300

    Add project files.

---
+++ WebApplication1/Migrations/20260224093831_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountLists",
+                columns: table => new
+                {
+                    AccountNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IBAN = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountLists", x => x.AccountNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountLists");
+        }
+    }
+}
---
+++ WebApplication1/Models/AccountList.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class AccountList
+    {
+        [Key]
+        public int Id { get; set; }
+        public String AccountNumber { get; set; } = string.Empty; //Hesap numarası
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Balance { get; set; } //Hesap bakiyesi
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal RemainingBalance { get; set; } //Kullanılabilir bakiye
+        public string IBAN { get; set;} = string.Empty; //Uluslararası banka hesap numarası
+        public string CurrencyCode { get; set; } = string.Empty; //Döviz kodu
+        public string AccountStatus { get; set; } = string.Empty; //Hesap durumu, A: Açık, K: Kapalı
+        public DateTime LastTransactionDate { get; set; } //Son işlem tarihi
+        public int AccountType { get; set; } //Hesap tipi
+    }
+}
