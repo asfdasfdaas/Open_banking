47af8cd556d4ca54c67130e69f8e5e0abbabda53
faruk <farukt9876@gmail.com>
2026-02-24 16:34:30 +0300

    Add Controller, first succesfull api test, able to get AccountList

---
+++ WebApplication1/Controllers/AccountListController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountListController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+
+        public AccountListController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountList>>> GetAll()
+        {
+            //unused code just in case
+            //
+            //return await _db.AccountLists.ToListAsync();
+
+            //var accountLists = _db.AccountLists.ToList();
+            //return Ok(accountLists);
+
+            var accounts = await _db.AccountLists.ToListAsync();
+
+            return Ok(accounts);
+
+            
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
