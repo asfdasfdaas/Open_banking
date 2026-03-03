6c53acd453e65b46e8a1e2d3686d0337e5618699
faruk <farukt9876@gmail.com>
2026-03-03 15:53:01 +0300

    dealing with problems :(

---
+++ WebApplication1/Migrations/20260303113140_AddProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProvider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderName",
+                table: "AccountLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderName",
+                table: "AccountLists");
+        }
+    }
+}
