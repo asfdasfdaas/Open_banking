30ca5c28300e3a83bd949552804b2f3e844f04a6
faruk <farukt9876@gmail.com>
2026-03-06 16:56:07 +0300

    trying to add tailwind

--- open-banking-ui/src/app/app.ts
+++ open-banking-ui/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { AuthService } from './services/auth';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('open-banking-ui');
+  title = signal('open-banking-ui');
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService, private router: Router) {
+    // Listen to the router. Every time the page changes, re-check the login status!
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoggedIn = this.authService.isLoggedIn();
+      }
+    });
+  }
+
+  // Helper method for the HTML to use
+  navigate(path: string) {
+    this.router.navigate([path]);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/']); // Kick them back to the home page
+  }
 }
